f456e8680c8587681e113343529e461e343b821f
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-28 15:32:43 -0400

    adds City model file with City struct, property wrapper structs, and getCities func

---
+++ parsing-JSON-practice/parsing-JSON-practice/Models/City.swift
@@ -0,0 +1,37 @@
+//
+//  City.swift
+//  parsing-JSON-practice
+//
+//  Created by Sunni Tang on 8/28/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+struct City: Codable {
+    let name: String
+    let main: CityTempWrapper
+    let weather: CityWeatherWrapper
+
+    static func getCities(from cityData: Data) throws -> [City] {
+        do {
+            let decoder = JSONDecoder()
+            let cities = try
+                decoder.decode([City].self, from: cityData)
+            return cities
+        } catch {
+            throw JSONError.decodingError(error)
+        }
+    }
+
+}
+
+
+
+struct CityTempWrapper: Codable {
+    let temp: Double
+}
+
+struct CityWeatherWrapper: Codable {
+    let description: String
+}
